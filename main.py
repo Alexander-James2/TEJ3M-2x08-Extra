ba5383a51f788ac1a37a2d9d5dd7d55c98ba2956
Alexander-James2 <144252140+Alexander-James2@users.noreply.github.com>
2023-12-01 11:18:14 -0500

    imports

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-# Created by Alex James (guide from arduino site) 
-# Created on Oct. 2023
-# rotates a servo with a potentiometer 
+# Alexander James
+# Dec. 2023
+# Uses a potentiometer to control a servo 
 
 import time
 import board
 import pwmio
 from adafruit_motor import servo
+from analogio import AnalogIn 
 
-Servo servoNumber1;  // create servo object 
-
-int potentiometer = 1;  // Sets potentiometer to analog pin1
-int value;    // variable to read the value from the analog pin
-
-void setup() {
-  servoNumber1.attach(2);  
-}
-
-void loop() {
-  value = analogRead(potentiometer);      // reads the value of the potentiometer 
-  value = map(value, 0, 1000, 0, 180);    // uses the map fucntion to change scale to degrees
-  servoNumber1.write(value);                  
-  delay(15);                        
-}
