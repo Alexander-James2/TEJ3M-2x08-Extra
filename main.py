868298aacc1ba88f334881d58309d688367a50ee
Alexander-James2 <144252140+Alexander-James2@users.noreply.github.com>
2023-12-05 10:59:37 -0500

    final code

--- main.py
+++ main.py
@@ -8,3 +8,14 @@ import pwmio
 from adafruit_motor import servo
 from analogio import AnalogIn 
 
+potentiometer = AnalogIn(board.A0)
+pwm = pwmio.PWMOut(board.A1, frequency=50)
+ServoNumber1 = servo.Servo(pwm)
+scale = 65535 / 180   # Takes the input from potentiometer and scales it to degrees.
+
+while True:
+    angle = potentiometer.value / scale
+    print (f"Angle: {angle}")
+    ServoNumber1.angle = angle 
+    time.sleep(0.1)
+    
